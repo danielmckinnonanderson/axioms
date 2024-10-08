291cd4204221a7885480f7bd647115df67f2f81b
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-10-07 19:29:07 -0500

    Improve bin form with the magic of javascript

--- src/main.rs
+++ src/main.rs
@@ -23,41 +23,28 @@ fn main() {
     let server_addr = format!("{}:{}", SERVER_HOSTNAME, SERVER_PORT);
     println!("Listening on {server_addr}");
 
-    let client_html_f = File::open("src/bin-form.html").expect("Couldn't find client HTML");
-    let client_html: String = {
-        let mut buf = String::new();
-
-        io::BufReader::new(client_html_f)
-            .read_to_string(buf.borrow_mut())
-            .expect("Couldn't read contents from client.html");
-
-        buf
-    };
-
     rouille::start_server(server_addr, move |request| {
         router!(request,
             (GET) (/) => {
+                let client_html_f = File::open("src/bin-form.html").expect("Couldn't find client HTML");
+                let client_html: String = {
+                    let mut buf = String::new();
+
+                    io::BufReader::new(client_html_f)
+                        .read_to_string(buf.borrow_mut())
+                        .expect("Couldn't read contents from client.html");
+
+                    buf
+                };
                 Response::html(client_html.as_str())
             },
            (GET) (/ws) => {
-                // This is the websockets route.
+                let (response, websocket) = try_or_400!(websocket::start(request, Some("axioms")));
 
-                // In order to start using websockets we call `websocket::start`.
-                // The function returns an error if the client didn't request websockets, in which
-                // case we return an error 400 to the client thanks to the `try_or_400!` macro.
-                //
-                // The function returns a response to send back as part of the `start_server`
-                // function, and a `websocket` variable of type `Receiver<Websocket>`.
-                // Once the response has been sent back to the client, the `Receiver` will be
-                // filled by rouille with a `Websocket` object representing the websocket.
-                let (response, websocket) = try_or_400!(websocket::start(request, Some("echo")));
-
-                // Because of the nature of I/O in Rust, we need to spawn a separate thread for
-                // each websocket.
+                // Spawn a separate thread for each websocket.
                 thread::spawn(move || {
-                    // This line will block until the `response` above has been returned.
+                    // Block until response resolves.
                     let ws = websocket.recv().unwrap();
-                    // We use a separate function for better readability.
                     websocket_handling_thread(ws);
                 });
 
@@ -68,7 +55,6 @@ fn main() {
     });
 }
 
-// Function run in a separate thread.
 fn websocket_handling_thread(mut websocket: websocket::Websocket) {
     // We wait for a new message to come from the websocket.
     while let Some(message) = websocket.next() {
